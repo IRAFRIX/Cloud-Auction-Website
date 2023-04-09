18c7cdf6f683992292a0a4ddb429a455abb6df88
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-04-09 15:44:05 +0700

    finish

--- src/handlers/login_handler.rs
+++ src/handlers/login_handler.rs
@@ -1,22 +1,43 @@
-use actix_web::{web, get, post, put, delete, Responder, HttpResponse};
+use actix_web::{web, post, HttpResponse};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use crate::models::account::account;
 
-/// POST method for `/login`
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginInfo {
+    userId: u64,
+    userPassword: u64,
+}
+
+fn authenticate(userId: u64, userPassword: u64) -> bool {
+    // implementation for authentication logic
+    true
+}
+
+fn get_user_info(userId: u64) -> account {
+    // implementation for retrieving user information from database
+    account {
+        userId: userId.to_string(),
+        userPassword: "password".to_string(),
+        userName: "User".to_string(),
+        userSurname: "Name".to_string(),
+    }
+}
+
+/// POST method for /login
 #[post("/login")]
 async fn login(
-    info: web::Json<String>,  // expecting JSON payload containing `userId` and `userPassword`
+    info: web::Json<LoginInfo>,  // expecting JSON payload containing userId and userPassword
 ) -> HttpResponse {
-    // assuming `LoginInfo` struct with `userId` and `userPassword` fields
-    let userId = 630305123456789;
-    let userPassword = 1234;
-    let userSurname = "user" ;
+    let userId = info.userId;
+    let userPassword = info.userPassword;
 
     // check if user credentials are correct
     let is_authenticated = authenticate(userId, userPassword);
 
     if is_authenticated {
-        // assuming `User` struct with `user_id` and `user_surname` fields
-        let user = user_info(userId);
+        // assuming User struct with user_id and user_surname fields
+        let account = get_user_info(userId);
         let response_body = json!({ "message": format!("welcome {}", account.userSurname) });
         HttpResponse::Ok().json(response_body)
     } else {
--- src/routes/login_route.rs
+++ src/routes/login_route.rs
@@ -1,7 +1,7 @@
 use actix_web::web;
-use crate::handlers::login_handler::login;
+use crate::handlers::login_handler::{login};
 
 pub fn config(cfg: &mut web::ServiceConfig){
     cfg
-    .service(login);
+        .service(login);
 }
