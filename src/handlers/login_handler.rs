025020af14b59739fd3fed3d6b2fb002030defb4
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-04-09 15:15:38 +0700

    loginrouteok

--- src/handlers/login_handler.rs
+++ src/handlers/login_handler.rs
@@ -4,20 +4,20 @@ use serde_json::json;
 /// POST method for `/login`
 #[post("/login")]
 async fn login(
-    info: web::Json<LoginInfo>,  // expecting JSON payload containing `userId` and `userPassword`
+    info: web::Json<String>,  // expecting JSON payload containing `userId` and `userPassword`
 ) -> HttpResponse {
     // assuming `LoginInfo` struct with `userId` and `userPassword` fields
-    let user_id = 630305123456789;
-    let user_password = 1234;
-    let user_surname = "user" ;
+    let userId = 630305123456789;
+    let userPassword = 1234;
+    let userSurname = "user" ;
 
     // check if user credentials are correct
-    let is_authenticated = authenticate(user_id, user_password);
+    let is_authenticated = authenticate(userId, userPassword);
 
     if is_authenticated {
         // assuming `User` struct with `user_id` and `user_surname` fields
-        let user = get_user_info(user_id);
-        let response_body = json!({ "message": format!("welcome {}", user.user_surname) });
+        let user = user_info(userId);
+        let response_body = json!({ "message": format!("welcome {}", account.userSurname) });
         HttpResponse::Ok().json(response_body)
     } else {
         let response_body = json!({ "message": "userId or userPassword incorrect" });
--- src/routes/login_route.rs
+++ src/routes/login_route.rs
@@ -2,5 +2,6 @@ use actix_web::web;
 use crate::handlers::login_handler::login;
 
 pub fn config(cfg: &mut web::ServiceConfig){
-
+    cfg
+    .service(login);
 }
