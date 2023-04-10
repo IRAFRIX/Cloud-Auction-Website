52960f76ef4797c655c32ba6ecc4ade9af87ef1b
bomcrmon <bomcrmon@gmail.com>
2023-04-10 07:14:46 +0700

    fix

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,26 +1,14 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, middleware};
-use env_logger::Env;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-pub mod routes;
-mod handlers;
-mod models;
-use crate::routes::{auction_route,account_route,login_route,register_route};
-
-
+#[get("/")]
+async fn index() -> impl Responder {
+   HttpResponse::Ok().body("Hello Jumpbox!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
-
    HttpServer::new(|| {
-       App::new()
-            .wrap(middleware::Logger::default())
-            .configure(auction_route::config)
-            .configure(account_route::config)
-            .configure(login_route::config)
-            .configure(register_route::config)
-
+       App::new().service(index)
    })
    .bind("127.0.0.1:8080")?
    .run()
